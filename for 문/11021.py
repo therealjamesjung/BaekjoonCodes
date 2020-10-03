788fef84228a32708f5c92694427688a934fb691
jamesj0918 <jamesj0107@naver.com>
2020-10-03 17:43:18 +0900

    11021.py

---
+++ for 문/11021.py
@@ -0,0 +1,7 @@
+from sys import stdin
+
+n = int(stdin.readline().rstrip())
+
+for i in range(n):
+    tmp = stdin.readline().rstrip().split()
+    print('Case #'+str(i+1)+': '+str(int(tmp[0])+int(tmp[1])))
