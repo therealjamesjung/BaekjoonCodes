de566d187eb378d116b935550939e719154b6d1e
jamesj0918 <jamesj0107@naver.com>
2020-10-07 23:45:19 +0900

    1929.py

---
+++ 수학 2/1929.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+def is_prime(n):
+    if n == 1:
+        return False
+    i=2
+    while i*i<=n:
+        if n%i == 0:
+            return False
+        i+=1
+    return True
+
+a = [int(x) for x in stdin.readline().rstrip().split(' ')]
+
+for x in range(a[0], a[1]+1):
+    if is_prime(x) is True:
+        print(x)
